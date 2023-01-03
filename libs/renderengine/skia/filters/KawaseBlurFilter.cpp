fc43659fb2e0358f27c167091fc97b8ebf1b10d4
minaripenguin <minaripenguin@users.noreply.github.com>
2023-01-04 02:03:54 +0800

    renderengine: skia: switch to glass blur effect
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    renderengine: Introduce optimized glass blur effect [1/2]
    
    * this CL aims to create a lighter and performance oriented blur rendering, but unlike stock that aims iOS blur, this one aims to produce a mirror/glass like blur effect (its like transparent with way less distortion compared to gaussian blur)
    
    Change-Id: I21f8050d13a6903ad15a95785c42114fe9dc7af0
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    renderengine: skia: Improve glass blur filter rendering
    
    * optimize memory allocation and remove double blurring in kawase filter
    
    Change-Id: Icc96a8fed3964378e52c0c1043e1c4d9c5ac264a
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- libs/renderengine/skia/filters/KawaseBlurFilter.cpp
+++ libs/renderengine/skia/filters/KawaseBlurFilter.cpp
@@ -20,6 +20,7 @@
 #include <SkAlphaType.h>
 #include <SkBlendMode.h>
 #include <SkCanvas.h>
+#include <SkImageFilters.h>
 #include <SkImageInfo.h>
 #include <SkPaint.h>
 #include <SkRRect.h>
@@ -87,13 +88,9 @@ sk_sp<SkImage> KawaseBlurFilter::generate(GrRecordingContext* context,
     // Kawase is an approximation of Gaussian, but it behaves differently from it.
     // A radius transformation is required for approximating them, and also to introduce
     // non-integer steps, necessary to smoothly interpolate large radii.
-    float tmpRadius = (float)blurRadius / 2.0f;
+    float tmpRadius = (float)blurRadius / 8.0f;
     uint32_t numberOfPasses = std::min(kMaxPasses, (uint32_t)ceil(tmpRadius));
     float radiusByPasses = tmpRadius / (float)numberOfPasses;
-
-    // create blur surface with the bit depth and colorspace of the original surface
-    SkImageInfo scaledInfo = input->imageInfo().makeWH(std::ceil(blurRect.width() * kInputScale),
-                                                       std::ceil(blurRect.height() * kInputScale));
 
     // For sampling Skia's API expects the inverse of what logically seems appropriate. In this
     // case you might expect Translate(blurRect.fLeft, blurRect.fTop) X Scale(kInverseInputScale)
@@ -105,8 +102,12 @@ sk_sp<SkImage> KawaseBlurFilter::generate(GrRecordingContext* context,
     SkSamplingOptions linear(SkFilterMode::kLinear, SkMipmapMode::kNone);
     SkRuntimeShaderBuilder blurBuilder(mBlurEffect);
     blurBuilder.child("child") =
-            input->makeShader(SkTileMode::kClamp, SkTileMode::kClamp, linear, blurMatrix);
+            input->makeShader(SkTileMode::kMirror, SkTileMode::kMirror, linear, blurMatrix);
     blurBuilder.uniform("in_blurOffset") = radiusByPasses * kInputScale;
+
+    // create blur surface with the bit depth and colorspace of the original surface
+    SkImageInfo scaledInfo = input->imageInfo().makeWH(std::ceil(blurRect.width() * kInputScale),
+                                                       std::ceil(blurRect.height() * kInputScale));
 
     constexpr int kSampleCount = 1;
     constexpr bool kMipmapped = false;
@@ -117,27 +118,30 @@ sk_sp<SkImage> KawaseBlurFilter::generate(GrRecordingContext* context,
     LOG_ALWAYS_FATAL_IF(!surface, "%s: Failed to create surface for blurring!", __func__);
     sk_sp<SkImage> tmpBlur = makeImage(surface.get(), &blurBuilder);
 
-    // And now we'll build our chain of scaled blur stages. If there is more than one pass,
-    // create a second surface and ping pong between them.
-    sk_sp<SkSurface> surfaceTwo;
-    if (numberOfPasses <= 1) {
-        LOG_ALWAYS_FATAL_IF(tmpBlur == nullptr, "%s: tmpBlur is null", __func__);
-    } else {
-        surfaceTwo = surface->makeSurface(scaledInfo);
-        LOG_ALWAYS_FATAL_IF(!surfaceTwo, "%s: Failed to create second blur surface!", __func__);
-
-        for (auto i = 1; i < numberOfPasses; i++) {
-            LOG_ALWAYS_FATAL_IF(tmpBlur == nullptr, "%s: tmpBlur is null for pass %d", __func__, i);
-            blurBuilder.child("child") =
-                    tmpBlur->makeShader(SkTileMode::kClamp, SkTileMode::kClamp, linear);
-            blurBuilder.uniform("in_blurOffset") = (float) i * radiusByPasses * kInputScale;
-            tmpBlur = makeImage(surfaceTwo.get(), &blurBuilder);
-            using std::swap;
-            swap(surface, surfaceTwo);
+   for (auto i = 1; i < numberOfPasses; i++) {
+        blurBuilder.child("child") =
+                tmpBlur->makeShader(SkTileMode::kMirror, SkTileMode::kMirror, linear);
+        blurBuilder.uniform("in_blurOffset") = (float) i * radiusByPasses * kInputScale;
+        sk_sp<SkImage> nextBlur = makeImage(surface.get(), &blurBuilder);
+        if (!nextBlur) {
+            return nullptr;
         }
+        tmpBlur = std::move(nextBlur);
     }
 
-    return tmpBlur;
+    const float sigmaScale = blurRadius * kInputScale * 0.5f;
+
+    SkPaint paint;
+    paint.setBlendMode(SkBlendMode::kSrc);
+
+    sk_sp<SkImageFilter> finalFilter = SkImageFilters::Blur(
+        sigmaScale, sigmaScale, SkTileMode::kMirror, nullptr);
+
+    paint.setImageFilter(finalFilter);
+
+    surface->getCanvas()->drawImage(tmpBlur.get(), 0, 0);
+
+    return surface->makeImageSnapshot();
 }
 
 } // namespace skia
